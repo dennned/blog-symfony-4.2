2ae9845c0b4cd15cc2f2e577f294f42a68e3d70c
Denis <makarenkodeniz@gmail.com>
2019-05-22 23:45:28 +0200

    add view page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Page;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -16,8 +17,27 @@ class PageController extends AbstractController
         $repoPages = $this->getDoctrine()->getRepository(Page::class);
         $pages = $repoPages->findAll();
 
-        return $this->render('page/list.html.twig', [
+        return $this->render('@page/list.html.twig', [
             'pages' => $pages,
+        ]);
+    }
+
+    /**
+     * @Route("/page/{id}", name="page_view")
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function viewAction(int $id)
+    {
+        $repoPages = $this->getDoctrine()->getRepository(Page::class);
+        $page = $repoPages->find($id);
+
+        if (!$page) {
+            throw $this->createNotFoundException('Page is not found!');
+        }
+        return $this->render('@page/view.html.twig', [
+            'page' => $page,
         ]);
     }
 }
